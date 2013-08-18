bd0f71300c2f934fcda8d6096315557797737bf3
Olav Stetter <olav.stetter@googlemail.com>
2013-08-18 15:50:46 +0200

    Fixed: Movie validation failed if year was blank.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,16 +1,21 @@
 class Movie < ActiveRecord::Base
   attr_accessible :description, :image_url, :title, :year
   
-  validates_presence_of :title, :year, :image_url
+  validates_presence_of :title, :image_url
   validates_uniqueness_of :title
-  validate :year_is_not_in_the_future
+  validate :year_is_valid
   
   private
   
-  def year_is_not_in_the_future
-    # Let's say we only accept movies that are already in cinemas, i.e. the year
-    # cannot be in the future.
-    if year > Time.now.year
+  def year_is_valid
+    if year.nil? or year.to_s.strip.blank?
+      errors.add(:year, "is empty or invalid")
+    elsif year.to_i < 1888
+      # Source for the beginning of movie history: http://en.wikipedia.org/wiki/History_of_film
+      errors.add(:year, "is too early, movies did not even exist back then :-)")
+    elsif year.to_i > Time.now.year
+      # Let's say we only accept movies that are already in cinemas, i.e. the year
+      # cannot be in the future.
       errors.add(:year, "cannot be in the future")
     end
   end
