9cac41730b23812a16d159511fdd6e5557286576
Olav Stetter <olav.stetter@googlemail.com>
2013-08-18 14:02:38 +0200

    Extracted passwords etc. to external file.
    Everything secret is now stored in `config/secrets.yml` which is of course not part of the git repository.

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,14 @@ module CinemaDB
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    # Load passwords etc. from YAML config file
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'secrets.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
+    
   end
 end
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,5 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-CinemaDB::Application.config.secret_token = '38518ee838f57566133e22e8a9acaba249fb877e333afde5280da7577f4473c9e17d5a4264410978415f30e1560a02d48f0ef8f6fe793a15045560bb6373d937'
+
+CinemaDB::Application.config.secret_token = ENV['RAILS_SECRET_TOKEN']
