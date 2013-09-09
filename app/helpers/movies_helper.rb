1a5d415cb8457388f4451eac0e84be7b916467de
Olav Stetter <olav.stetter@googlemail.com>
2013-09-09 17:56:08 +0200

    Filters are now created with backend data (DRY).

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -5,6 +5,33 @@ module MoviesHelper
     return Category.possible_categories.collect{ |c| [c.to_s, c.to_i] }
   end
   
+  def category_filter_list_JS
+    full_hash = Hash.new
+    full_hash['All genres'] = -1
+    Category.possible_categories.each{|cat| full_hash[cat.to_s] = cat.to_i}
+    return full_hash.collect do |title, value|
+      "{name: '#{title}', value: #{value}}"
+    end.join(", ").html_safe
+  end
+  
+  def years_filter_list_JS(y_start, y_end)
+    full_hash = Hash.new
+    full_hash['All years'] = -1
+    (y_start..y_end).each{|y| full_hash[y.to_s] = y}
+    return full_hash.collect do |title, value|
+      "{name: '#{title}', value: #{value}}"
+    end.join(", ").html_safe
+  end
+  
+  def scores_filter_list_JS(s_start, s_end)
+    full_hash = Hash.new
+    full_hash['All scores'] = -1
+    (s_start..s_end).each{|s| full_hash["#{pluralize s, 'star'}"] = s}
+    return full_hash.collect do |title, value|
+      "{name: '#{title}', value: #{value}}"
+    end.join(", ").html_safe
+  end
+  
   def score_class_stars(sc)
     return "\u2605"*sc.to_i + "\u2606"*(5 - sc.to_i)
   end
